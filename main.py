95568fea7a7e615bc37fe09d620ee4847fa7b07d
REclipsent <wmtinf01@infinitymec.com>
2023-12-01 18:33:50 -0600

    Initial

---
+++ main.py
@@ -0,0 +1,10 @@
+
+class Board:
+    def __init__(self, playerOneIcon: str, playerTwoIcon: str, isSinglePlayer: bool):
+        self.playerOneIcon = playerOneIcon
+        self.playerTwoIcon = playerTwoIcon
+        self.isSinglePlayer = isSinglePlayer
+
+
+if __name__ == '__main__':
+    pass
